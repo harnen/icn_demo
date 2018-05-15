5ee586af00b677a795ac9d6f64b7fe565be93894
Michał Król <mharnen@gmail.com>
2018-05-15 15:37:34 +0100

    Adding OCR implementation

---
+++ ocr/ocr.h
@@ -0,0 +1,7 @@
+#ifndef OCR_H
+#define OCR_H
+
+void character_recognition_wrap(int** input, int rows, int cols, int** letters_c, int letters_rows,
+    char *output_letters, int *length);
+
+#endif /* OCR_H */
